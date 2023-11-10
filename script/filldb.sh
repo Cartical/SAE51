b741008cf9351bb582dc05b82af027c8690f8fc7
Cartical <cartierc27@gmail.com>
2023-11-10 13:35:17 +0100

    Tous est fonctionnel sauf quelque bug d'auto increment ;)

--- script/filldb.sh
+++ script/filldb.sh
@@ -29,3 +29,64 @@ do
     mysql -u $DB_USER -p$DB_PASS -h $DB_HOST $DB_NAME --port=3307 -e "$query"
     
 done
+
+tail -n +2 "./csv/marque.csv" | while IFS=";" read -r nom;
+do
+    # Requête SQL d'insertion pour chaque ligne du CSV
+    query="INSERT INTO marque (nom) VALUES ('$nom');"
+
+    # Exécution de la requête via la commande mysql
+    mysql -u $DB_USER -p$DB_PASS -h $DB_HOST $DB_NAME --port=3307 -e "$query"
+    
+done
+
+tail -n +2 "./csv/logiciels.csv" | while IFS=";" read -r nom version;
+do
+    # Requête SQL d'insertion pour chaque ligne du CSV
+    query="INSERT INTO logiciels (nom, version) VALUES ('$nom', '$version');"
+
+    # Exécution de la requête via la commande mysql
+    mysql -u $DB_USER -p$DB_PASS -h $DB_HOST $DB_NAME --port=3307 -e "$query"
+    
+done
+
+tail -n +2 "./csv/technicien.csv" | while IFS=";" read -r prenom nom;
+do
+    # Requête SQL d'insertion pour chaque ligne du CSV
+    query="INSERT INTO technicien (prenom, nom) VALUES ('$prenom', '$nom');"
+
+    # Exécution de la requête via la commande mysql
+    mysql -u $DB_USER -p$DB_PASS -h $DB_HOST $DB_NAME --port=3307 -e "$query"
+    
+done
+
+
+tail -n +2 "./csv/pc.csv" | while IFS=";" read -r date_achat id_marque id_ram id_os id_logiciel;
+do
+    # Requête SQL d'insertion pour chaque ligne du CSV
+    query="INSERT INTO pc (date_achat, id_marque, id_ram, id_os, id_logiciel) VALUES ('$date_achat', '$id_marque', '$id_ram', '$id_os', '$id_logiciel');"
+
+    # Exécution de la requête via la commande mysql
+    mysql -u $DB_USER -p$DB_PASS -h $DB_HOST $DB_NAME --port=3307 -e "$query"
+    
+done
+
+tail -n +2 "./csv/maintenance.csv" | while IFS=";" read -r description date id_pc id_technicien;
+do
+    # Requête SQL d'insertion pour chaque ligne du CSV
+    query="INSERT INTO maintenance (description, date, id_pc, id_technicien) VALUES ('$description', '$date', '$id_pc', '$id_technicien');"
+
+    # Exécution de la requête via la commande mysql
+    mysql -u $DB_USER -p$DB_PASS -h $DB_HOST $DB_NAME --port=3307 -e "$query"
+    
+done
+
+tail -n +2 "./csv/users.csv" | while IFS=";" read -r prenom nom id_pc;
+do
+    # Requête SQL d'insertion pour chaque ligne du CSV
+    query="INSERT INTO users (prenom, nom, id_pc) VALUES ('$prenom', '$nom', '$id_pc');"
+
+    # Exécution de la requête via la commande mysql
+    mysql -u $DB_USER -p$DB_PASS -h $DB_HOST $DB_NAME --port=3307 -e "$query"
+    
+done
