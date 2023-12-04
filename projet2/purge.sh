ad06f183c747cbcab9687bca4b3fce32d7f0bb88
Cartical <cartierc27@gmail.com>
2023-12-04 15:03:28 +0100

    test import csv fichier dump sql

--- projet2/purge.sh
+++ projet2/purge.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-mysql -u dolibarr -p'dolibarr' -h 127.0.0.1 --port=3306 dolibarr < DROP DATABASE dolibarr;
+mysql -u dolibarr -p'dolibarr' -h 127.0.0.1 --port=3306 dolibarr < "sql/purge.sql" 2> /dev/null
 
 docker stop mysql-cont
 docker stop dolibarr-cont
@@ -9,3 +9,5 @@ docker rm mysql-cont
 docker rm dolibarr-cont
 
 docker network rm sae51
+
+sudo service mysql stop
