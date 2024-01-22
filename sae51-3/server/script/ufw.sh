424ea034fb3010cc73c6f7dfb3165dc9554c92ea
Cartical <cartierc27@gmail.com>
2024-01-22 18:21:52 +0100

    modif instructions d'installations

--- sae51-3/server/script/ufw.sh
+++ sae51-3/server/script/ufw.sh
@@ -35,7 +35,6 @@ ufw allow 443 #https
 #ufw allow 22 #ssh
 ufw allow from 10.0.2.2 to 10.0.2.15 port 22 # autoriser le ssh sur le serveur lui même
 
-
 ### redémarrage de ufw et affichge du status des règles après execution du script.
 ufw --force reload
 ufw status verbose
