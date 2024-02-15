8e7ba217c0f728a18406fce411a3e3f9ffb47aa6
Cartical <calvin.cartier@univ-rouen.fr>
2024-02-15 14:04:07 +0000

    conteneur ok app + html ok a voir bdd

--- SAE61/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS demosql;
-USE demosql;
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
-);
-
--- SAE61/purge.sh
+++
@@ -1,5 +0,0 @@
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-
-docker system prune
-docker volume prune
